6db1d21048d1895bda3d86f4374e55eecb94d90b
Pontus Lindman <pontus.v.lindman@gmail.com>
2025-12-30 14:43:18 +0200

    build tools verify details func

--- install/installers/devtools-01-tools.sh
+++ install/installers/devtools-01-tools.sh
@@ -5,3 +5,19 @@ APP_COMMAND=("make" "rg")
 install_tools() {
     install_and_show_versions build-essential ripgrep
 }
+
+verify_details_tools() {
+    local missing=()
+    
+    if ! command_exists "make"; then
+        missing+=("build-essential (make)")
+    fi
+    
+    if ! command_exists "rg"; then
+        missing+=("ripgrep (rg)")
+    fi
+
+    if [ ${#missing[@]} -gt 0 ]; then
+        print_color "$YELLOW" "   Missing: ${missing[*]}"
+    fi
+}
