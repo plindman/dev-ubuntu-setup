0d22328c4a2d2a941f140647be48cd1bb7bfcd2e
Pontus Lindman <pontus.v.lindman@gmail.com>
2026-01-20 23:23:17 +0200

    Removed dotfiles (replaced by chezmoi and dotfiles repo)

--- dotfiles/oh-my-zsh/custom/aliases.zsh
+++
@@ -1,7 +0,0 @@
-# Added --js-runtimes bun to force it to use Bun
-alias youtube-dl="yt-dlp --js-runtimes bun"
-alias ytdl="yt-dlp --js-runtimes bun"
-
-alias ytdl-update="yt-dlp -U"
-
-alias yle-dl="uvx yle-dl"
