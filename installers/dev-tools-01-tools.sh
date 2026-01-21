ed27b2b2d3b3fcbab533074b26ad067fe5db400a
Pontus Lindman <pontus.v.lindman@gmail.com>
2026-01-21 20:11:22 +0200

    only installs build-essential (renamed to "Build Essentials")

--- installers/dev-tools-01-tools.sh
+++ installers/dev-tools-01-tools.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
-APP_NAME="CLI Development Tools (Build Essentials, rg, fzf)"
-APP_COMMAND=("make" "rg" "fzf")
+APP_NAME="Build Essentials"
+APP_COMMAND=("make" "gcc" "g++")
 
 install_tools() {
-    quiet_apt_install build-essential ripgrep fzf
+    quiet_apt_install build-essential
 }
 
 verify_details_tools() {
     local missing=()
-    
+
     if ! command_exists "make"; then
         missing+=("build-essential (make)")
     fi
-    
-    if ! command_exists "rg"; then
-        missing+=("ripgrep (rg)")
-    fi
 
-    if ! command_exists "fzf"; then
-        missing+=("fzf")
+    if ! command_exists "gcc"; then
+        missing+=("build-essential (gcc)")
     fi
 
     if [ ${#missing[@]} -gt 0 ]; then
